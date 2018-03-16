6e5c4eee4f5206bf9bad4561ae8ff567c4a31dc5
normanlo <normanlo@users.noreply.github.com>
2018-03-16 15:04:01 -0400

    Add files via upload

--- Phase 4/src/AvailableItems.java
+++ Phase 4/src/AvailableItems.java
@@ -1,3 +1,10 @@
+/*
+    This program creates a file holding information on the ongoing auctions. The file will store auction information on
+    the item name, seller username, bidder username, minimum bid, and number of days remaining.
+    The program allows for quick changes and retrieval of the auction information.
+    The back end will apply the daily transactions from the daily transaction file to this file
+    to create a new available items file for next day.
+*/
 public class AvailableItems {
 
     public String itemName;
@@ -6,6 +13,11 @@ public class AvailableItems {
     public float minBid;
     public int numDays;
 
+    /*
+        This constructor method sets variables based on the provided arguments.
+        The variables store information on the item name, seller username,
+        bidder username, minimum bid, and number of days remaining.
+    */
     public AvailableItems(String item, String sName, String bName, float bidAmount, int days){
         itemName = item;
         sellerName = sName;
@@ -14,48 +26,61 @@ public class AvailableItems {
         numDays = days;
     }
 
+    // This set method sets the name of the item being auctioned.
     public void setItemName(String item){
         itemName = item;
     }
 
+    // This set method sets the username of the item's seller.
     public void setSellerName(String sName){
         sellerName = sName;
     }
 
+    // This set method sets the username of the item's current highest bidder.
     public void setBuyerName(String bName){
         buyerName = bName;
     }
 
+    // This set method sets the minimum bid required to bid for the item.
     public void setBidAmount(float bidAmount){
         minBid = bidAmount;
     }
 
+    // This set method sets the number of days that the auction will last for.
     public void setNumDays(int days){
         numDays = days;
     }
 
 
+    // This get method returns the name of the item being auctioned.
     public String getItemName(){
         return itemName;
     }
 
+    // This get method returns the username of the item's seller.
     public String getSellerName(){
         return sellerName;
     }
 
+    // This get method returns the username of the item's current highest bidder.
     public String getBuyerName(){
         return buyerName;
     }
 
+    // This get method returns the minimum bid amount required for the item.
     public float getBidAmount(){
         return minBid;
     }
 
+    // This get method returns the number of remaining days of the bid.
     public int getNumDays(){
         return numDays;
     }
 
-
+    /*
+        This method returns a string consisting of the item name, seller username,
+        bidder username, number of days remaining, and minimum bid.
+    */
     public String toString(){
         return itemName + "," + sellerName + "," + buyerName + "," + String.valueOf(numDays) + "," + String.valueOf(minBid);
     }
--- Phase 4/src/UserAccounts.java
+++ Phase 4/src/UserAccounts.java
@@ -1,47 +1,69 @@
+/*
+    This program creates a file holding information on the user accounts. The file will store account information on
+    the user type, username and available credit.The program allows for quick changes
+    and retrieval of the account information. The back end will apply the daily
+    transactions from the daily transaction file to this file to create a new user account file for next day.
+*/
 public class UserAccounts {
 
     public String userType;
     public String userName;
     public float availableCredit;
 
+    /*
+        This constructor method sets variables based on the provided arguments.
+        The variables store information on the username, user type and available credit.
+    */
     public UserAccounts(String name, String type, float credit) {
         userName = name;
         userType = type;
         availableCredit = credit;
     }
 
+    // This set method sets the user account's type.
     public void setUserType(String type) {
         userType = type;
     }
 
+    // This set method sets the username.
     public void setUserName(String name) {
         userName = name;
     }
 
+    // This set method sets the user's available credit.
     public void setAvailableCredit(float credit) {
         availableCredit = credit;
     }
 
+    // This method decreases the available credit amount by the bid amount.
     public void bidItem(float bidAmount) {
         availableCredit -= bidAmount;
     }
 
+    // This method increases the available credit amount by the bid amount.
     public void advertiseItem(float bidAmount) {
         availableCredit += bidAmount;
     }
 
+    // This get method returns the user account's type.
     public String getUserType() {
         return userType;
     }
 
+    // This get method returns the username.
     public String getUsername() {
         return userName;
     }
 
+    // This get method returns the user's available credit.
     public double getAvailableCredit() {
         return availableCredit;
     }
 
+    /*
+        This method returns a string consisting of the user type, username and the
+        available credit.
+    */
     public String toString(){
         return userType + "," + userName + "," + String.valueOf(availableCredit);
     }
